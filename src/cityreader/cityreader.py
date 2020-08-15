b2922e2a21c21edcbe42819327a61649390bd51c
Amber-Pittman <amber@amberpittman.com>
2020-08-14 20:29:21 -0400

    Cityreader passing tests. MVP met.

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -14,6 +14,19 @@
 #
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
+import csv
+
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
+
+  def __repr__(self):
+    return f"City: {self.name}, Latitude: {self.lat}, Longitude: {self.lon} "
+
+
+
 cities = []
 
 def cityreader(cities=[]):
@@ -21,14 +34,28 @@ def cityreader(cities=[]):
   # Ensure that the lat and lon valuse are all floats
   # For each city record, create a new City instance and add it to the 
   # `cities` list
+  with open("src/cityreader/cities.csv", newline="") as c:
+    csv_reader = csv.reader(c, delimiter=",")
+    line_count = 0
+
+    for row in csv_reader:
+      if line_count == 0:
+        line_count += 1
+        pass
+      else:
+        cities.append(City(row[0], float(row[3]), float(row[4])))
+        line_count += 1
+    # Print the list of cities (name, lat, lon), 1 record per line.
+    for c in cities:
+        print(c)
+
+    print(f"\n Number of Lines Processed: {line_count}\n\n")
     
     return cities
 
 cityreader(cities)
 
-# Print the list of cities (name, lat, lon), 1 record per line.
-for c in cities:
-    print(c)
+
 
 # STRETCH GOAL!
 #
